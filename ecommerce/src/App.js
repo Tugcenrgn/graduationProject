b5faba5fd056c9b5aa219a6db59bf110d1f27261
Tuğçe Nur Ergen <tugcenurergen9@gmail.com>
2023-02-13 20:29:52 +0300

    update

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ ecommerce/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="App">
+       Home page
+    </div>
+
+  );
+}
+
+
+export default Home;
